d0db1d772dd7d032bf9bcae242cccd85998fc398
Raiden <raiden.naylor@outlook.com>
2024-09-20 12:45:49 +0100

    Create main.py
    started development of main code block

---
+++ main.py
@@ -0,0 +1,34 @@
+#libaries 
+from ohbot import ohbot
+import random
+import os
+import ffmpeg
+
+
+#test block (commands)
+#ohbot.reset()
+#ohbot.move(ohbot.HEADNOD, 10)
+#ohbot.wait(1)
+#ohbot.move(ohbot.HEADTURN, 10)
+#ohbot.wait(1)
+#ohbot.move(ohbot.EYETRUN, 10)
+#ohbot.wait(1)
+#ohbot.move(ohbot.LIDBLINK, 10)
+#ohbot.wait(1)
+#ohbot.move(ohbot.TOPLIP, 10)
+#ohbot.wait(1)
+#ohbot.move(ohbot.BOTTOMLIP, 10)
+#ohbot.wait(1)
+#ohbot.move(ohbot.EYETILT, 10)
+#ohbot.wait(1)
+#ohbot.say("Hello World")
+#ohbot.wait(1)
+#ohbot.reset()
+
+#main block - 
+ohbot.reset()
+
+def main():
+    ohbot.move(ohbot.HEADNOD, 10)
+    ohbot.wait(1)
+
